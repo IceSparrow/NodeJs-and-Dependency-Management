5adfc0e15609055d9fab621c2ee49d3203350957
Aislu Baisova <ice_lu_93@mail.ru>
2022-07-20 19:03:35 +0300

    Practical task

---
+++ 1.js
@@ -0,0 +1,35 @@
+//console.log ('That is' + ' ' + true); 
+//console.log ('I am' + ' '+ 29);
+//console.log (29 + true);
+
+//console.log ('That is ' * true); 
+//console.log ('I am ' * 29);
+//console.log (29 * true);
+
+//console.log ('That is ' / true); 
+//console.log ('I am ' / 29);
+//console.log (29 / true);
+
+let a = '123'
+num = Number(a)
+console.log(num)
+
+let b = '123'
+boo = Boolean(b)
+console.log(boo)
+
+let c = 2
+word = String(c)
+console.log(word)
+
+let d = 2
+boo2 = Boolean(d)
+console.log(boo2)
+
+let e = false
+what = String(e)
+console.log(what)
+
+let f = true
+why = Number(f)
+console.log(why)
