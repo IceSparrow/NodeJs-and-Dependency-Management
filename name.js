2891d4c91555d86b687489cac670f9c5762a13f2
Aislu Baisova <ice_lu_93@mail.ru>
2022-07-22 17:34:46 +0300

    Add another task

---
+++ name.js
@@ -0,0 +1,8 @@
+function inTerminal(name) {
+if (name == 'Aislu') {
+    console.log('Hello ' + name + '!') }
+    else console.log('There is no such name!')
+}
+
+inTerminal('Oleg')
+inTerminal('Aislu')
